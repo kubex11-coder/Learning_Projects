0cc61a4a1d309bb4407563bd61dbe3de833219b7
kubex11-coder <jakubdvorak91@seznam.cz>
2021-05-04 14:22:06 +0200

    development

---
+++ Travel_goals/scripts/goals.js
@@ -0,0 +1,11 @@
+const mobile = document.getElementsByClassName ('mobile') [0]
+const Menu = document.getElementsByClassName ('nav_menu') [0]
+
+mobile.addEventListener ('click',()=> {
+    Menu.classList.toggle('active');
+})
+
+
+/*Responzive Navbar tutorial
+https://www.youtube.com/watch?v=At4B7A4GOPg
+*/
