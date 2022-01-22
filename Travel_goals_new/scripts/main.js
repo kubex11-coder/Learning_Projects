b5d8f586e85456a5d33df3e802b49a6a9b51092a
kubex11-coder <jakubdvorak91@seznam.cz>
2022-01-22 14:25:59 +0100

    re-design

--- Travel_goals_new/scripts/main.js
+++ Travel_goals_new/scripts/main.js
@@ -1,10 +1,12 @@
 const mobile = document.getElementsByClassName ('mobile') [0]
 const Menu = document.getElementsByClassName ('right_side_nav') [0]
 const Hero = document.getElementsByClassName('header') [0]
+const Language = document.getElementsByClassName("language") [0]
 
 mobile.addEventListener ('click',()=> {
     Menu.classList.toggle('active');
-    Hero.classList.toggle('active')
+    Hero.classList.toggle('active');
+    Language.classList.toggle("active")
 })
 
 
