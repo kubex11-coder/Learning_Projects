763d3b6d8064d92aa70d6c5c66ac4d8165e455d0
kubex11-coder <80459898+kubex11-coder@users.noreply.github.com>
2022-06-10 14:54:52 +0200

    preklady develop

---
+++ Preklady/script/script.js
@@ -0,0 +1,7 @@
+const hamburgerMenu = document.getElementById("hamburger");
+const mobilePopUp = document.querySelector(".mobile_pop_up");
+
+hamburgerMenu.addEventListener("click", () => {
+mobilePopUp.classList.toggle("active");
+
+})
