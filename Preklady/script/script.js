9b54f58d74fd0a251550d362af59b4dde61d844c
kubex11-coder <80459898+kubex11-coder@users.noreply.github.com>
2022-08-21 17:09:45 +0200

    dev

--- Preklady/script/script.js
+++ Preklady/script/script.js
@@ -5,3 +5,10 @@ hamburgerMenu.addEventListener("click", () => {
 mobilePopUp.classList.toggle("active");
 
 })
+
+// <!-- Android Viewport height fix for virtual keybord in form field-->
+
+var isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1; //&& ua.indexOf("mobile");
+if(isAndroid) {
+    document.write('<meta name="viewport" content="width=device-width,height='+window.innerHeight+', initial-scale=1.0">');
+}
